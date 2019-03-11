f562de5cabef2aff93c99b574e13ee4b2c5fd012
tapakkur <2514252875@qq.com>
2019-03-11 21:45:07 +0800

    add some user functions

--- src/main/java/com/qakmak/eshop/controller/UserController.java
+++ src/main/java/com/qakmak/eshop/controller/UserController.java
@@ -7,6 +7,7 @@ import com.qakmak.eshop.service.UserServiceImpl;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -23,15 +24,37 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/qakmak")
 public class UserController {
 
-//    @Autowired
-//    private UserService userService;
-@Autowired
-private UserServiceImpl userService;
+    @Autowired
+    private UserServiceImpl userService;
 
-    public void saveUser(User user){}
-    public void deleteUser(int userId){}
-    public void updateUser(User user){}
+    @ApiOperation(value = "添加用户", notes = "添加用户信息")
+    @ApiImplicitParams({
+            @ApiImplicitParam(paramType = "query", name = "user", value = "用户")
+    })
+    @RequestMapping(value = "saveUser/", method = RequestMethod.POST)
+    public void saveUser(@RequestParam(value = "user") User user){
+        userService.saveUser(user);
+    }
 
+    @ApiOperation(value = "删除用户", notes = "删除用户信息")
+    @ApiImplicitParams({
+            @ApiImplicitParam(paramType = "query", name = "userId", value = "用户id", required = true)
+    })
+    @RequestMapping(value = "deleteUser/", method = RequestMethod.POST)
+    public void deleteUser(@RequestParam(value = "userId", required = true) Integer userId){
+        userService.deleteUser(userId);
+    }
+
+    @ApiOperation(value = "更新用户", notes = "更新用户信息")
+    @ApiImplicitParams({
+            @ApiImplicitParam(paramType = "query", name = "user", value = "用户")
+    })
+    @RequestMapping(value = "updateUser/", method = RequestMethod.POST)
+    public void updateUser(User user){
+        userService.updateUser(user);
+    }
+
+    @ApiOperation(value = "按用户ID查询", notes = "按用户ID查询具体的用户信息")
     @ApiImplicitParams({
             @ApiImplicitParam(paramType = "query", name = "userId", value = "user id", required = true)
     })
@@ -40,11 +63,19 @@ private UserServiceImpl userService;
                               ){
         return userService.queryByUserId(userId);
     }
+
+    @ApiOperation(value = "查询所用用户信息", notes = "查询所用用户信息")
     @RequestMapping(value = "/queryAll", method = RequestMethod.POST)
     public PageInfo<User> queryAll(){
         return userService.queryAll();
     }
-    public PageInfo<User> fuzzyQuery(String key){
-        return null;
+
+    @ApiOperation(value = "模糊查询", notes = "按用户名模糊查询")
+    @ApiImplicitParams({
+            @ApiImplicitParam(paramType = "query", name = "key", value = "user name", required = true)
+    })
+    @RequestMapping(value = "fuzzyQuery/", method = RequestMethod.POST)
+    public PageInfo<User> fuzzyQuery(@RequestParam(value = "key") String key){
+        return userService.fuzzyQuery(key);
     }
 }
